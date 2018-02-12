ffd0aed3ec16a81c51d457503e0e88ba0abf9008
Vinceboxx2 <36300934+Vinceboxx2@users.noreply.github.com>
2018-02-12 21:54:11 +0100

    Update DepDao.java

--- src/cn/itcast/invoice/auth/dep/dao/dao/DepDao.java
+++ src/cn/itcast/invoice/auth/dep/dao/dao/DepDao.java
@@ -3,17 +3,19 @@ package cn.itcast.invoice.auth.dep.dao.dao;
 import cn.itcast.invoice.auth.dep.vo.DepModel;
 import cn.itcast.invoice.util.base.BaseDao;
 /**
- * this interface extends BaseDao
+ * this interface extends BaseDao  
  *
  */
 public interface DepDao extends BaseDao<DepModel> {
 }
-/*
-public void save(DepModel dm);
-public void update(DepModel dm);
+/*   gf
+public void save(DepModel dm); 
+public void update(DepModel dm); f
+f 
 public void delete(DepModel dm);
-public DepModel get(Long uuid);
+
+ public DepModel get(Long uuid);
 public List<DepModel> getAll();
-public List<DepModel> getAll(DepQueryModel dqm,Integer pageNum,Integer pageCount);
+ public List<DepModel> getAll(DepQueryModel dqm,Integer pageNum,Integer pageCount);
 public Integer getCount(DepQueryModel dqm);
 */
