7ef899eb2e9605b5a8011881a56d071565112831
Vinceboxx2 <36300934+Vinceboxx2@users.noreply.github.com>
2018-02-12 21:53:17 +0100

    Update DepEbi.java

--- src/cn/itcast/invoice/auth/dep/business/ebi/DepEbi.java
+++ src/cn/itcast/invoice/auth/dep/business/ebi/DepEbi.java
@@ -6,7 +6,7 @@ import cn.itcast.invoice.auth.dep.vo.DepModel;
 import cn.itcast.invoice.util.base.BaseEbi;
 
 /**
- * this is an interface based on BaseEbi
+ * this is an interface based on DepEbi
  *
  */
 @Transactional
@@ -16,23 +16,23 @@ public interface DepEbi extends BaseEbi<DepModel> {
 
 /*
 public void save(DepModel dm);
-
+   
 public void update(DepModel dm);
-
+ 
 public void delete(DepModel dm);
-
+ 
 public DepModel get(Long uuid);
-
+ 
 public List<DepModel> getAll();
 
-/**
- * åˆ†é¡µèŽ·å�–æ•°æ�®
- * @param dqm æŸ¥è¯¢æ�¡ä»¶
- * @param pageNum é¡µç �å€¼
- * @param pageCount æ¯�é¡µæ˜¾ç¤ºæ•°
+/** 
+ * Ã¥Ë†â€ Ã©Â¡ÂµÃ¨Å½Â·Ã¥ï¿½â€“Ã¦â€¢Â°Ã¦ï¿½Â®
+ * @param dqm Ã¦Å¸Â¥Ã¨Â¯Â¢Ã¦ï¿½Â¡Ã¤Â»Â¶
+ * @param pageNum Ã©Â¡ÂµÃ§Â ï¿½Ã¥â‚¬Â¼
+ * @param pageCount Ã¦Â¯ï¿½Ã©Â¡ÂµÃ¦ËœÂ¾Ã§Â¤ÂºÃ¦â€¢Â°
  * @return
- */
+ */ 
 /*	public List<DepModel> getAll(DepQueryModel dqm, Integer pageNum,Integer pageCount);
-
-public Integer getCount(DepQueryModel dqm);
+      
+public Integer getCount(DepQueryModel dqm); 
 */
